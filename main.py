1e9c72d69cd0bc27644ea69d419f28f92ca632ad
Rachel Lee <rachel318318@gmail.com>
2021-03-14 18:30:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ Created on Sun Mar 14 15:04:20 2021
 """
 
 import pandas as pd
+
+database = pd.read_csv("/Users/rachellee/Documents/GitHub/Genshin_project/database_ex/Sheet 1-Table 1.csv")
+
+print(database)
