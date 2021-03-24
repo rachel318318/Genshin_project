ebbc43408fb1d053c004235e7eaa5a92b06a0196
Rachel Lee <rachel318318@gmail.com>
2021-03-24 16:19:10 -0400

    updated

--- database.py
+++ database.py
@@ -23,22 +23,23 @@ def leveling(source):
     else:
         lst_complete = [i for i in lst if i != ' ']
 
+    level_lst = [i for i in lst_complete if i[-1] == '0' and len(i) == 2]
 
     level = []
     materials = []
     mora = []
-    
+
     i = 0
     while i < len(lst_complete):
         if '000' in lst_complete[i]:
             s = lst_complete[i].replace(',', '')
             mora.append(int(s))
             i += 1
-        elif lst_complete[i][-1] == '0' and len(lst_complete[i]) == 2:
+        elif lst_complete[i] in level_lst:
             level.append(int(lst_complete[i]))
             i += 1
             dic = {}
-            while lst_complete[i][-1] != '0' and len(lst_complete[i]) != 2:
+            while lst_complete[i] not in level_lst and '000' not in lst_complete[i]:
                 if lst_complete[i] == 'X':
                     i += 1
                 else:
@@ -48,7 +49,7 @@ def leveling(source):
             materials.append(dic)
         else:
             i += 1
-    
+
     d = {}
     d[lst_complete[0]] = level
     d[lst_complete[1]] = materials
@@ -79,16 +80,61 @@ def character_and_weapon(source):
     for key in all_weapon_links.keys():
         all_character_links.pop(key)
 
+    dict_weapon_char = char_to_weapon(source)
+
     lst_leveling = []
     for i in range(len(all_character_links)):
         leveling_df = leveling(list(all_character_links.values())[i])
         leveling_df['캐릭터'] = list(all_character_links.keys())[i]
+        leveling_df['무기'] = dict_weapon_char[list(all_character_links.keys())[i]]
         lst_leveling.append(leveling_df)
-        print(lst_leveling)
     
     lst_leveling_complete = pd.concat(lst_leveling)
     lst_leveling_complete['캐릭터'] = lst_leveling_complete['캐릭터'].replace('\(원신\)','',regex=True)
     return lst_leveling_complete
+
+def char_to_weapon(source):
+    webpage_response = requests.get(source)
+
+    webpage = webpage_response.content
+
+    soup = BeautifulSoup(webpage, 'html.parser')
+
+    tbody_lst = []
+    for i in soup.find_all('tbody'):
+        tbody_lst.append(i)
+
+    weapon_div = None
+    for i in tbody_lst:
+        div = i.find_all('div')
+        for j in div:
+            if "무기" in j.get_text():
+                weapon_div = i
+
+    tr = weapon_div.find_all('tr')
+    weapon_char_lst = []
+    for i in tr[1:]:
+        td = i.find_all('td')
+        for j in td:
+            new_lst2 = []
+            a = j.find_all(attrs = {'class':'wiki-link-internal'})
+            for t in a:
+                if '원신/무기/' in t.attrs['title'] and t.attrs['title'] not in weapon_char_lst:
+                    weapon_char_lst.append(t.attrs['title'].replace('원신/무기/', ''))
+                elif t.attrs['title'] not in new_lst2:
+                    new_lst2.append(t.attrs['title'])
+            if new_lst2:
+                weapon_char_lst.append(new_lst2)
+
+
+    j = 1
+    weapon_char_dict = {}
+    while j < len(weapon_char_lst):
+        for k in weapon_char_lst[j]:
+            weapon_char_dict[k] = weapon_char_lst[j-1]
+        j += 2
+    
+    return weapon_char_dict
 
 def all_data(source):
     main_site_response = requests.get(source)
@@ -104,7 +150,7 @@ def all_data(source):
     return result
 
 
-df_all_data = all_data('https://namu.wiki/w/%EC%9B%90%EC%8B%A0')
+df_all_data = all_data('https://namu.wiki/w/%EC%9B%90%EC%8B%A0').reset_index()
 
 df_all_data = df_all_data[['무기','캐릭터','돌파 레벨','캐릭터 육성 소재','모라']]
 
--- test.py
+++ test.py
@@ -3,47 +3,57 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 
-webpage_response = requests.get("https://namu.wiki/w/%ED%8B%80:%EC%9B%90%EC%8B%A0%20%ED%94%8C%EB%A0%88%EC%9D%B4%EC%96%B4%EB%B8%94%20%EC%BA%90%EB%A6%AD%ED%84%B0")
+webpage_response = requests.get("https://namu.wiki/w/%EA%B0%90%EC%9A%B0")
 
 webpage = webpage_response.content
 
 soup = BeautifulSoup(webpage, 'html.parser')
 
+all_tables = soup.select('.wiki-table')
+
 lst = []
-for i in soup.find_all('tbody'):
-    lst.append(i)
+for i in all_tables:
+    if '돌파 레벨' in i.get_text():
+        lst = i.get_text('|').split("|")
 
-new = None
-for i in lst:
-    div = i.find_all('div')
-    for j in div:
-        if "무기" in j.get_text():
-            new = i
+lst_complete = []
+if not lst:
+    pass
+else:
+    lst_complete = [i for i in lst if i != ' ']
 
-tr = new.find_all('tr')
-new_lst = []
-for i in tr[1:]:
-    td = i.find_all('td')
-    for j in td:
-        new_lst2 = []
-        a = j.find_all(attrs = {'class':'wiki-link-internal'})
-        for t in a:
-            if '원신/무기/' in t.attrs['title'] and t.attrs['title'] not in new_lst:
-                new_lst.append(t.attrs['title'].replace('원신/무기/', ''))
-            elif t.attrs['title'] not in new_lst2:
-                new_lst2.append(t.attrs['title'])
-        if new_lst2:
-            new_lst.append(new_lst2)
+level_lst = [i for i in lst_complete if i[-1] == '0' and len(i) == 2]
 
+level = []
+materials = []
+mora = []
 
-j = 1
-new_dict = {}
-while j < len(new_lst):
-    for k in new_lst[j]:
-        new_dict[k] = new_lst[j-1]
-    j += 2
+i = 0
+while i < len(lst_complete):
+    if '000' in lst_complete[i]:
+        s = lst_complete[i].replace(',', '')
+        mora.append(int(s))
+        i += 1
+    elif lst_complete[i] in level_lst:
+        level.append(int(lst_complete[i]))
+        i += 1
+        dic = {}
+        while lst_complete[i] not in level_lst and '000' not in lst_complete[i]:
+            if lst_complete[i] == 'X':
+                i += 1
+            else:
+                num = lst_complete[i+1].replace('×', '').replace(' ', '')
+                dic[lst_complete[i]] = int(num)
+                i += 2
+        materials.append(dic)
+    else:
+        i += 1
 
-characters_weapon = ""
-# for i in weapon:
-#     if i.get_text() in weapon_types:
-#         characters_weapon = i.get_text()
+d = {}
+d[lst_complete[0]] = level
+d[lst_complete[1]] = materials
+d[lst_complete[2]] = mora
+
+df_leveling = pd.DataFrame(data=d)
+
+print(df_leveling)
