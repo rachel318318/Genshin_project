e136fc49e45fecb4fc54a0aec68ec6ab733add59
Rachel Lee <rachel318318@gmail.com>
2021-03-23 23:18:24 -0400

    Update database.py

--- database.py
+++ database.py
@@ -57,58 +57,77 @@ def leveling(source):
     df_leveling = pd.DataFrame(data=d)
     return df_leveling
 
-def all_data(source):
-    main_site_response = requests.get(source)
-    main_site = main_site_response.content
 
-    main_soup = BeautifulSoup(main_site, 'html.parser')
+def character_and_weapon(source):
+    character_site_response = requests.get(source)
+    character_site = character_site_response.content
 
-    #all_links_html = weapon_soup.find_all('a', href = True)
-    #all_links_html = weapon_soup.find_all(href = re.compile('^/w/%'))
-    
-    ## Goes into 캐릭터 link
+    character_soup = BeautifulSoup(character_site, 'html.parser')
 
-    character_links_html = main_soup.find_all('a', string = "캐릭터")
-    weapon_links_html = main_soup.find_all('a', string = "무기")
+    all_links_html = character_soup.find_all(attrs = {'class':'wiki-link-internal'})
 
-    print(character_links_html)
-    print(weapon_links_html)
-
-    all_links = {}
+    all_character_links = {}
     for i in all_links_html:
-        if i.string != None and i.string != '원신/등장인물':
-            all_links[i.string] = 'https://namu.wiki'+i['href']
+        all_character_links[i.attrs['title']] = 'https://namu.wiki'+i['href']
+    all_character_links.pop('원신')
+    all_character_links.pop('원신/등장인물')
+
+    all_weapon_links = {}
+    for key in all_character_links.keys():
+        if '원신/무기/' in key:
+            all_weapon_links[key] = all_character_links[key]
     
+    for key in all_weapon_links.keys():
+        all_character_links.pop(key)
+
     lst_leveling = []
-    for i in range(len(all_links)):
-        leveling_df = leveling(list(all_links.values())[i])
-        leveling_df['캐릭터'] = list(all_links.keys())[i]
+    for i in range(len(all_character_links)):
+        leveling_df = leveling(list(all_character_links.values())[i])
+        leveling_df['캐릭터'] = list(all_character_links.keys())[i]
+        leveling_df['무기'] = weapon_type(list(all_character_links.values())[i])
         lst_leveling.append(leveling_df)
     
     lst_leveling_complete = pd.concat(lst_leveling)
     lst_leveling_complete['캐릭터'] = lst_leveling_complete['캐릭터'].replace('\(원신\)','',regex=True)
     return lst_leveling_complete
 
-all_data('https://namu.wiki/w/%EC%9B%90%EC%8B%A0')
+def weapon_type(source):
+    weapon_types = ['한손검','양손검','장병기','법구','활']
+
+    webpage_response = requests.get(source)
+
+    webpage = webpage_response.content
+
+    soup = BeautifulSoup(webpage, 'html.parser')
+
+    weapon = soup.select(".wiki-table wiki-dynamic-96dce1fd5b899b7cc760c1f8e1bdb1c8")
+
+    characters_weapon = ""
+    for i in weapon:
+        if i.get_text() in weapon_types:
+            characters_weapon = i.get_text()
+    
+    return characters_weapon
 
 
-#weapon_types = ['Swords','Claymores','Polearms','Catalysts','Bows']
-# weapon_types = ['한손검','양손검','장병기','법구','활']
-# weapon_types_sites = [
-#     'https://namu.wiki/w/%EB%B6%84%EB%A5%98:%EC%9B%90%EC%8B%A0/%ED%94%8C%EB%A0%88%EC%9D%B4%EC%96%B4%EB%B8%94%20%EC%BA%90%EB%A6%AD%ED%84%B0/%ED%95%9C%EC%86%90%EA%B2%80',
-#     'https://namu.wiki/w/%EB%B6%84%EB%A5%98:%EC%9B%90%EC%8B%A0/%ED%94%8C%EB%A0%88%EC%9D%B4%EC%96%B4%EB%B8%94%20%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%96%91%EC%86%90%EA%B2%80',
-#     'https://namu.wiki/w/%EB%B6%84%EB%A5%98:%EC%9B%90%EC%8B%A0/%ED%94%8C%EB%A0%88%EC%9D%B4%EC%96%B4%EB%B8%94%20%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%9E%A5%EB%B3%91%EA%B8%B0',
-#     'https://namu.wiki/w/%EB%B6%84%EB%A5%98:%EC%9B%90%EC%8B%A0/%ED%94%8C%EB%A0%88%EC%9D%B4%EC%96%B4%EB%B8%94%20%EC%BA%90%EB%A6%AD%ED%84%B0/%EB%B2%95%EA%B5%AC',
-#     'https://namu.wiki/w/%EB%B6%84%EB%A5%98:%EC%9B%90%EC%8B%A0/%ED%94%8C%EB%A0%88%EC%9D%B4%EC%96%B4%EB%B8%94%20%EC%BA%90%EB%A6%AD%ED%84%B0/%ED%99%9C'
-# ]
+def all_data(source):
+    main_site_response = requests.get(source)
+    main_site = main_site_response.content
 
-# lst_all_data = []
-# for i in range(len(weapon_types_sites)):
-#     df_each_weapon = all_data_each_weapon(weapon_types_sites[i])
-#     df_each_weapon['무기'] = weapon_types[i]
-#     lst_all_data.append(df_each_weapon)
+    main_soup = BeautifulSoup(main_site, 'html.parser')
 
-# df_all_data = pd.concat(lst_all_data, ignore_index=True)
-# df_all_data = df_all_data[['무기','캐릭터','돌파 레벨','캐릭터 육성 소재','모라']]
+    character_weapon_link_html = main_soup.find_all('a', string = "캐릭터")
+    character_weapon_link = 'https://namu.wiki' + character_weapon_link_html[0]['href']
 
-# df_all_data.to_csv('Leveling_guide.csv')
+    result = character_and_weapon(character_weapon_link)
+
+    return result
+
+
+df_all_data = all_data('https://namu.wiki/w/%EC%9B%90%EC%8B%A0')
+
+df_all_data = df_all_data[['무기','캐릭터','돌파 레벨','캐릭터 육성 소재','모라']]
+
+df_all_data.to_csv('Leveling_guide.csv')
+
+# weapon_types = ['Swords','Claymores','Polearms','Catalysts','Bows']
