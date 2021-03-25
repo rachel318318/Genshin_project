91835b7e34ef131b0baf71869638bdbdbc707e18
Rachel Lee <rachel318318@gmail.com>
2021-03-24 21:58:32 -0400

    added weapon leveling guide

--- database.py
+++ database.py
@@ -55,6 +55,9 @@ def leveling(source):
     d[lst_complete[1]] = materials
     d[lst_complete[2]] = mora
 
+    global glevel
+    glevel = level
+
     df_leveling = pd.DataFrame(data=d)
     return df_leveling
 
@@ -81,17 +84,25 @@ def character_and_weapon(source):
         all_character_links.pop(key)
 
     dict_weapon_char = char_to_weapon(source)
-
     lst_leveling = []
     for i in range(len(all_character_links)):
         leveling_df = leveling(list(all_character_links.values())[i])
         leveling_df['캐릭터'] = list(all_character_links.keys())[i]
-        leveling_df['무기'] = dict_weapon_char[list(all_character_links.keys())[i]]
+        leveling_df['무기 종류'] = dict_weapon_char[list(all_character_links.keys())[i]]
         lst_leveling.append(leveling_df)
     
+    lst_weapon_leveling = []
+    for i in range(len(all_weapon_links)):
+        weapon_leveling_df = weapon_leveling(list(all_weapon_links.values())[i])
+        weapon_leveling_df['무기 종류'] = list(all_weapon_links.keys())[i]
+        lst_weapon_leveling.append(weapon_leveling_df)
+    
+    weapon_leveling_complete = pd.concat(lst_weapon_leveling)
+    weapon_leveling_complete['무기 종류'] = weapon_leveling_complete['무기 종류'].replace('원신/무기/','',regex=True)
+
     lst_leveling_complete = pd.concat(lst_leveling)
     lst_leveling_complete['캐릭터'] = lst_leveling_complete['캐릭터'].replace('\(원신\)','',regex=True)
-    return lst_leveling_complete
+    return lst_leveling_complete, weapon_leveling_complete
 
 def char_to_weapon(source):
     webpage_response = requests.get(source)
@@ -136,6 +147,100 @@ def char_to_weapon(source):
     
     return weapon_char_dict
 
+def weapon_leveling(source):
+    webpage_response = requests.get(source)
+
+    webpage = webpage_response.content
+
+    soup = BeautifulSoup(webpage, 'html.parser')
+
+    all_names = soup.select('h3')
+
+    weapon_name_lst = []
+    for i in all_names:
+        ele = i.get_text()
+        ele = re.sub('\d+\.\d+\. ','',ele)
+        weapon_name_lst.append(ele.replace('[편집]',''))
+
+    all_tables = soup.select('tr')
+    ascending_html_lst = []
+    for i in all_tables:
+        if '돌파 소재' in i.get_text():
+            ascending_html_lst.append(i)
+
+    leveling_num = []
+    leveling_item = []
+    for i in ascending_html_lst:
+        lst2 = []
+        lst3 = []
+        lst4 = []
+        for j in i.find_all(attrs = {'style':'display:inline'}):
+            lst2.append(j.get_text())
+        leveling_num.append(lst2)
+
+        for k in i.find_all(attrs = {'title':'원신/육성 아이템'}):
+            lst3.append(k.get_text())
+            lst4 = [i for i in lst3 if i]
+        leveling_item.append(lst4)
+
+    mora = []
+    for i in range(len(leveling_num)):
+        lst5 = []
+        j = 3
+        while j < len(leveling_num[i]):
+            if leveling_num[i][j] == '0':
+                lst5.append(int(leveling_num[i][j]))
+                j += 4
+            else:
+                ele = leveling_num[i][j].replace(',','')
+                lst5.append(int(ele))
+                j += 4
+        mora.append(lst5)
+
+    leveling_lst = []
+    for i in range(len(leveling_item)):
+        inner_leveling_lst = []
+        leveling_dic = {}
+        j = 0
+        k = 0
+        while j < len(leveling_num[i]):
+            if j % 4 == 3:
+                j += 1
+            elif k % 3 == 0:
+                if k != 0:
+                    inner_leveling_lst.append(leveling_dic)
+                leveling_dic = {}
+                leveling_dic[leveling_item[i][k]] = leveling_num[i][j]
+                j += 1
+                k += 1
+            else:
+                leveling_dic[leveling_item[i][k]] = leveling_num[i][j]
+                j += 1
+                k += 1
+        inner_leveling_lst.append(leveling_dic)
+        leveling_lst.append(inner_leveling_lst)
+
+    levels = []
+    for i in range(len(mora)):
+        inner_levels = []
+        for j in range(len(mora[i])):
+            inner_levels.append(glevel[j])
+        levels.append(inner_levels)
+
+    data_complete_lst = []
+    for i in range(len(mora)):
+        data_complete_dic = {}
+        data_complete_dic['레벨'] = levels[i]
+        data_complete_dic['소재'] = leveling_lst[i]
+        data_complete_dic['모라'] = mora[i]
+        data_complete_df = pd.DataFrame(data=data_complete_dic)
+        data_complete_df['무기 이름'] = weapon_name_lst[i]
+        data_complete_lst.append(data_complete_df)
+    
+    data_complete = pd.concat(data_complete_lst)
+
+    return data_complete
+
 def all_data(source):
     main_site_response = requests.get(source)
     main_site = main_site_response.content
@@ -145,15 +250,17 @@ def all_data(source):
     character_weapon_link_html = main_soup.find_all('a', string = "캐릭터")
     character_weapon_link = 'https://namu.wiki' + character_weapon_link_html[0]['href']
 
-    result = character_and_weapon(character_weapon_link)
+    char_result, weapon_result = character_and_weapon(character_weapon_link)
 
-    return result
+    return char_result, weapon_result
 
 
-df_all_data = all_data('https://namu.wiki/w/%EC%9B%90%EC%8B%A0').reset_index()
+df_char_all_data = all_data('https://namu.wiki/w/%EC%9B%90%EC%8B%A0')[0].reset_index()
+df_char_all_data = df_char_all_data[['무기 종류','캐릭터','돌파 레벨','캐릭터 육성 소재','모라']]
+df_char_all_data.to_csv('Leveling_guide.csv')
 
-df_all_data = df_all_data[['무기','캐릭터','돌파 레벨','캐릭터 육성 소재','모라']]
-
-df_all_data.to_csv('Leveling_guide.csv')
+df_weapon_all_data = all_data('https://namu.wiki/w/%EC%9B%90%EC%8B%A0')[1].reset_index()
+df_weapon_all_data = df_weapon_all_data[['무기 종류','무기 이름','레벨','소재','모라']]
+df_weapon_all_data.to_csv('Weapon_leveling_guide.csv')
 
 # weapon_types = ['Swords','Claymores','Polearms','Catalysts','Bows']
--- test.py
+++ test.py
@@ -3,17 +3,99 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 
+glevel = [20, 40, 50, 60, 70, 80]
+
 webpage_response = requests.get("https://namu.wiki/w/%EC%9B%90%EC%8B%A0/%EB%AC%B4%EA%B8%B0/%ED%95%9C%EC%86%90%EA%B2%80")
 
 webpage = webpage_response.content
 
 soup = BeautifulSoup(webpage, 'html.parser')
 
-all_tables = soup.select('h3')
+all_names = soup.select('h3')
 
 weapon_name_lst = []
-for i in all_tables:
+for i in all_names:
     ele = i.get_text()
     ele = re.sub('\d+\.\d+\. ','',ele)
     weapon_name_lst.append(ele.replace('[편집]',''))
 
+all_tables = soup.select('tr')
+ascending_html_lst = []
+ascending_text = []
+for i in all_tables:
+    text2 = []
+    if '돌파 소재' in i.get_text():
+        ascending_html_lst.append(i)
+
+leveling_num = []
+leveling_item = []
+for i in ascending_html_lst:
+    lst2 = []
+    lst3 = []
+    lst4 = []
+    for j in i.find_all(attrs = {'style':'display:inline'}):
+        lst2.append(j.get_text())
+    leveling_num.append(lst2)
+
+    for k in i.find_all(attrs = {'title':'원신/육성 아이템'}):
+        lst3.append(k.get_text())
+        lst4 = [i for i in lst3 if i]
+    leveling_item.append(lst4)
+
+mora = []
+for i in range(len(leveling_num)):
+    lst5 = []
+    j = 3
+    while j < len(leveling_num[i]):
+        if leveling_num[i][j] == '0':
+            lst5.append(int(leveling_num[i][j]))
+            j += 4
+        else:
+            ele = leveling_num[i][j].replace(',','')
+            lst5.append(int(ele))
+            j += 4
+    mora.append(lst5)
+
+leveling_lst = []
+for i in range(len(leveling_item)):
+    inner_leveling_lst = []
+    leveling_dic = {}
+    j = 0
+    k = 0
+    while j < len(leveling_num[i]):
+        if j % 4 == 3:
+            j += 1
+        elif k % 3 == 0:
+            if k != 0:
+                inner_leveling_lst.append(leveling_dic)
+            leveling_dic = {}
+            leveling_dic[leveling_item[i][k]] = leveling_num[i][j]
+            j += 1
+            k += 1
+        else:
+            leveling_dic[leveling_item[i][k]] = leveling_num[i][j]
+            j += 1
+            k += 1
+    inner_leveling_lst.append(leveling_dic)
+    leveling_lst.append(inner_leveling_lst)
+
+levels = []
+for i in range(len(mora)):
+    inner_levels = []
+    for j in range(len(mora[i])):
+        inner_levels.append(glevel[j])
+    levels.append(inner_levels)
+
+data_complete_lst = []
+for i in range(len(mora)):
+    data_complete_dic = {}
+    data_complete_dic['레벨'] = levels[i]
+    data_complete_dic['소재'] = leveling_lst[i]
+    data_complete_dic['모라'] = mora[i]
+    data_complete_df = pd.DataFrame(data=data_complete_dic)
+    data_complete_df['무기 이름'] = weapon_name_lst[i]
+    data_complete_lst.append(data_complete_df)
+
+data_complete = pd.concat(data_complete_lst)
+
+print(data_complete)
